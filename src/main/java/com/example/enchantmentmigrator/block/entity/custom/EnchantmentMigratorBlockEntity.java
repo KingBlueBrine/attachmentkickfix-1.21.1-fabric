b30e5f5b5613c5fce788a2285d298ec841705786
Robbie <temp.lgmyrek@gmail.com>
2025-12-23 19:01:52 +0000

    Dec 23, 2025, 8:01 PM

--- src/main/java/com/example/enchantmentmigrator/block/entity/custom/EnchantmentMigratorBlockEntity.java
+++ src/main/java/com/example/enchantmentmigrator/block/entity/custom/EnchantmentMigratorBlockEntity.java
@@ -256,7 +256,7 @@ public class EnchantmentMigratorBlockEntity extends BlockEntity implements Imple
                         int particleCount = isDragon ? 24 : 8;
                         double radius = 1.2;
                         //double angularSpeed = 0.25; // radians per tick
-                        double tangentialSpeed = 0.02;
+                        double tangentialSpeed = 0.03;
 
                         //double cx = pos.getX() + 0.5;
                         //double cy = isDragon ? pos.getY() + 0.5 : pos.getY() + 1;
@@ -265,14 +265,14 @@ public class EnchantmentMigratorBlockEntity extends BlockEntity implements Imple
 
                         for (int i = 0; i < particleCount; i++) {
                             double t = isDragon ? (double) i / particleCount : 0;
-                            double theta = isDragon ? (t * Math.PI * 2) + Math.toRadians(rotation) : (2 * Math.PI / particleCount) * i;
+                            double theta = isDragon ? (t * Math.PI * 2) /*+ Math.toRadians(rotation)*/ : (2 * Math.PI / particleCount) * i;
 
                             double cx = end.x+ (Math.cos(theta) * radius);
                             double cz = end.z+ (Math.sin(theta) * radius);
                             double cy = isDragon ? end.y + t * 1.5 : end.y+(Math.sin(theta) * 0.05);
 
-                            double vx = isDiamond ? 0 : (Math.sin(theta) * tangentialSpeed) + (world.random.nextGaussian() * 0.03);
-                            double vz = isDiamond ? 0 : (-Math.cos(theta) * tangentialSpeed) + (world.random.nextGaussian() * 0.03);
+                            double vx = isDiamond ? 0 : (Math.sin(theta) * tangentialSpeed) /*+ (world.random.nextGaussian() * 0.03)*/;
+                            double vz = isDiamond ? 0 : (-Math.cos(theta) * tangentialSpeed) /*+ (world.random.nextGaussian() * 0.03)*/;
                             double vy = isDragon ? 0.04 : 0;
 
                             //Vec3d start = Vec3d.add(x, x, vz, z)
